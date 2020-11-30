d4fee00601a6acc38242920b93c77fcd98bb4ffc
luchianm <mihai.luchian04@gmail.com>
2020-11-30 19:28:37 +0200

    first backend release

--- modules/auth.js
+++ modules/auth.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const axios = require('axios');
 const mailgun = require('mailgun-js');
+const joi=require('joi');
 
 const router = express.Router();
 
 router.post('/login', login);
 
-async function login (req, res){
+const schema = joi.object({
+    username: joi
+        .string()
+        .alphanum()
+        .min(3)
+        .max(50)
+        .required(),
+    password: joi
+        .string()
+        .min(3)
+        .max(50)
+        .required()
+});
+
+async function login ({body}, res){
     const url = 'http://localhost:5984/_session';
-    const { data } = await axios.post(url,req.body);
+    await schema.validateAsync(body);
+    const { data } = await axios.post(url,body);
     console.log(data);
     res.send(data);
     const email = {
